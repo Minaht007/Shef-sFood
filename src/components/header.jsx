8a64b480a6acb80a75c73f38f011c9cd606adbce
Andrey <108063187+Minaht007@users.noreply.github.com>
2024-10-13 15:20:47 +0300

    12345

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -148,7 +148,7 @@ const Header = () => {
 
       {isModalOpen && (
         <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center" >
-          <div className="bg-white p-4 rounded-lg w-[30%] h-min">
+          <div className="bg-white p-4 rounded-lg sm:w-[80%] w-[30%] h-min">
             <ModalWind onClose={() => setIsModalOpen(false)}>
               <h1 className="flex flex-row justify-center my-5 text-2xl">
                 Ваше замовлення
