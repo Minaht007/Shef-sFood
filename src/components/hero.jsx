7092a1eef310285a9375f0af444905b7d8634279
Andrey <108063187+Minaht007@users.noreply.github.com>
2024-10-13 15:17:28 +0300

    hero text

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
     return (
         <div className="bg-heroBgImg w-screen h-[600px] bg-top bg-no-repeat layout relative ">
             <div className="flex items-center relative" >
-                <p className="flex desk:w-[400px]  text-5xl leading-[64px] text-mainTextColor text-center font-bold absolute pt-[30%] left-[10%]  ">ВИ КУПЛЯЄТЕ <br /> МИ <br /> ДОСТАВЛЯЄМО</p>
+                <p className="flex desk:w-[400px] sm:text-2xl md:text-3xl  desk:text-5xl leading-[64px] text-mainTextColor text-center font-bold absolute pt-[30%] left-[10%]  ">ВИ КУПЛЯЄТЕ <br /> МИ <br /> ДОСТАВЛЯЄМО</p>
             </div>
 
 
